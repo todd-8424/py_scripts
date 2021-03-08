f1ab4a0aa5b9e816a491db4fad053a9d7164235b
Todd Kearney <todd.kearney8424@gmail.com>
2021-03-08 16:43:35 -0500

    testing

--- morpheus_dump.py
+++ morpheus_dump.py
@@ -3,8 +3,9 @@ import sys
 import json
 from pprint import pprint
 
-print(sys.executable)
+morpheus = 
 
+print(sys.executable)
 print("OS Env:")
 # j_env = json.loads(os.environ)
 # j_env_str = json.dumps(j_env, indent=4)
@@ -17,3 +18,4 @@ pprint(morpheus)
 # j_morpheus = json.loads(morpheus)
 # j_morpheus_str = json.dumps(j_morpheus, indent=4)
 # print(j_morpheus_str)
+print(json.dumps(morpheus, ensure_ascii=False, indent=4))
