749ece470763b792c072b158b2a55ec1c8363b32
Todd Kearney <todd.kearney8424@gmail.com>
2021-03-08 14:53:26 -0500

    Adding AD query

---
+++ ad_query.py
@@ -0,0 +1,40 @@
+
+#!/Users/tkearney/Development/py-3.9/bin/python
+
+from ldap3 import Server, Connection, ALL, NTLM
+import sys
+import getpass
+import argparse
+
+# Vars
+server=""
+ad_user=""
+search_base=""
+domain=""
+search_filter="(&(objectclass=user))"
+
+
+def getArgs():
+    global server
+    global ad_user
+    global domain
+    parser=argparse.ArgumentParser(description='Connect to AD and search OU for all users')
+    parser.add_argument('-domain', help='Please supply the domain')
+    parser.add_argument('-user', help='Please supply username in "domain\\user" format' )
+    parser.add_argument('-server', help='Supply server to connect to')
+    parser.add_argument('-search', help='Supply search DN')
+    args = parser.parse_args()
+    server = args.server
+    ad_user = args.user
+    domain = args.domain
+    return server, ad_user, domain
+
+getArgs()
+
+ad_user="\""+domain+"\\"+ad_user+"\""
+ad_password = getpass.getpass(prompt='Please enter password for '+ad_user)
+server = Server(server, get_info=ALL)
+con = Connection(server, ad_user, ad_password, authentication=NTLM, auto_bind=True)
+con.search(search_base,"(objectClass=user)", attributes=['sn', 'displayName', 'objectclass'])
+print(con.entries)
+con.unbind()
