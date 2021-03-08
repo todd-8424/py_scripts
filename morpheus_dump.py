1d487329cbe5b45e7bd568d1ed57c9155a6beb49
Todd Kearney <todd.kearney8424@gmail.com>
2021-03-08 15:51:58 -0500

    testing printing

--- morpheus_dump.py
+++ morpheus_dump.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from pprint import pprint
 
 print(sys.executable)
@@ -9,4 +10,8 @@ pprint(os.environ)
 print("sys.argv:")
 pprint(sys.argv)
 print("morpheus:")
-pprint(morpheus) 
+pprint(morpheus)
+
+j_morpheus = json.loads(morpheus)
+j_morpheus_str = json.dumps(j_morpheus, indent=4)
+print(j_morpheus_str)
