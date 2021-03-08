3bf278b088cd210f334dfa57f442667a2dabe651
Todd Kearney <todd.kearney8424@gmail.com>
2021-03-08 16:15:13 -0500

    Updated again

--- morpheus_dump.py
+++ morpheus_dump.py
@@ -3,16 +3,17 @@ import sys
 import json
 from pprint import pprint
 morpheus=""
-
 print(sys.executable)
 
 print("OS Env:")
 j_env = json.loads(os.environ)
-j_env_str = json.dumps(os.environ, indent=4)
+j_env_str = json.dumps(j_env, indent=4)
 print(j_env_str)
-# print("sys.argv:")
-# pprint(sys.argv)
+print("sys.argv:")
+pprint(sys.argv)
 print("morpheus:")
+pprint(morpheus)
+
 j_morpheus = json.loads(morpheus)
 j_morpheus_str = json.dumps(j_morpheus, indent=4)
 print(j_morpheus_str)
